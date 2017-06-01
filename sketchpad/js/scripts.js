c0ccae2ecb212f6aa1cfe0a77e21236253a7e081
David Williams <thedavesignal@gmail.com>
2017-06-01 15:17:24 -0700

    Finished project to specifications in Odin Project assignment.

--- sketchpad/js/scripts.js
+++ sketchpad/js/scripts.js
@@ -1,13 +1,28 @@
-var numBoxes = 16;
-var hovColor = "rgb(208, 212, 219)"
-var normColor = $("body").css("background-color");
 $(document).ready(function(){
-  for (i=0; i< numBoxes;i++){
-    var offset=i+1;
-    console.log("Test " + offset);
-    $("#sketchpad").append("<div class='canvas'></div>");
-  }
-  $('.canvas').hover(function(){
-    $(this).css("background-color", hovColor);
+  var initSize = 4;
+  var hovColor = "rgb(0, 0, 0)"
+  var normColor = $("body").css("background-color");
+  var defineCanvas = function(canvasSide){
+    var canvasArea = canvasSide * canvasSide;
+    var cellSide = $("#sketchpad").width()/canvasSide;
+    return [canvasArea, cellSide];
+  };
+  var buildCanvas = function(canvasArray){
+    $("#sketchpad").empty();
+    for (i=0; i< canvasArray[0];i++){
+      $("#sketchpad").append("<div class='canvas'></div>");
+    }
+    $(".canvas").css("width",canvasArray[1]);
+    $(".canvas").css("height",canvasArray[1]);
+    $('.canvas').hover(function(){
+      $(this).css("background-color", hovColor);
+    });
+  };
+  var drawingSpace = defineCanvas(initSize);
+  buildCanvas(drawingSpace);
+  $("#reset-button").click(function(){
+    $(".canvas").css("background-color",normColor);
+    drawingSpace = defineCanvas(prompt("How many squares per side on your new canvas?"));
+    buildCanvas(drawingSpace);
   });
 });
